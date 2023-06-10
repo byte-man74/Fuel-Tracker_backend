4bd060b9557c64c5a38fa8867e0cdc0d85cab660
Sheryf Habib <sheryf.534@gmail.com>
2023-06-10 02:46:36 +0100

    Added the fueling station modal here. - the foreign key relationships hasn't been established here yet

--- Backend/Main/models.py
+++ Backend/Main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Fueling_station(models.Model):
+    name = models.CharField( max_length=50)
+    logo = models.ImageField( upload_to="Fueling_station_Image_directory")
+    background_image = models.ImageField( upload_to="Fueling_station_Image_directory")
+    #! Add Price here (foreign key) 
+    #! Add Traffic_rating here (foreign key)
+    #! Add Position here (foreign key)
+    #! Add Extra_info here (foreign key)
+
+
