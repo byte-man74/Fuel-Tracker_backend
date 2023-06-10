2953ee01c1a756ebcd7306009f3c2a053ab81066
Sheryf Habib <sheryf.534@gmail.com>
2023-06-10 11:52:40 +0100

    added inline distribution to fuel admin

--- Backend/Main/admin.py
+++ Backend/Main/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class Fuel_station_price_inline(admin.StackedInline):
+    model = Fuel_Station_Price
+    can_delete = False
+    verbose_name_plural = 'Fuel Station Price'
+
+
+class Fuel_station_position_inline(admin.StackedInline):
+    model = Fuel_Station_Position
+    can_delete = False
+    verbose_name_plural = 'Fuel Station Position'
+
+
+class Fuel_Station_Traffic_Rating_inline(admin.StackedInline):
+    model = Fuel_Station_Traffic_Rating
+    can_delete = False
+    verbose_name_plural = 'Fuel Station Traffic Rating'
+
+
+class Fuel_Station_Extra_Information_inline(admin.StackedInline):
+    model = Fuel_Station_Extra_Information
+    can_delete = False
+    verbose_name_plural = 'Fuel Station Extra Informtion'
+
+
+
+class Fueling_station_Admin(admin.ModelAdmin):
+    inlines = (Fuel_station_price_inline, Fuel_station_position_inline,
+               Fuel_Station_Traffic_Rating_inline, Fuel_Station_Extra_Information_inline)
+
+
+admin.site.register(Fueling_station, Fueling_station_Admin)
