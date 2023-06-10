7c280a662a3f8a595f9e2e2b458ecdc563e21bea
Sheryf Habib <sheryf.534@gmail.com>
2023-06-10 09:21:49 +0100

    Added null and defaault fields to the existing models

--- Backend/Main/models.py
+++ Backend/Main/models.py
@@ -17,8 +17,8 @@ class Fueling_station(models.Model):
     
 
 class Fuel_Station_Price (models.Model):
-    amount = models.BigIntegerField()
-    votes = models.IntegerField()
+    amount = models.BigIntegerField(null=True)
+    votes = models.IntegerField(default=0)
     last_updated = models.DateTimeField( auto_now=False, auto_now_add=False) #! find out the model that changes this field on edit of the model class
 
 
@@ -30,10 +30,20 @@ class Fuel_Station_Price (models.Model):
     def __str__(self):
         return self.name
     
+class Fuel_Station_Traffic_Rating (models.Model):
+    terrible_rating_count = models.IntegerField(default=0)
+    average_rating_count = models.IntegerField(default=0)
+    good_rating_count = models.IntegerField(default=0)
+
+    #!find a way of grabbing the fuel station and representing it in a name here
+    def __str__(self):
+        return self.name
+    
+    # todo find a way of returning the percentages of the number of votes
 
 class Fuel_Station_Position (models.Model):
-    longitude = models.CharField( max_length=200)
-    latitude = models.CharField( max_length=200)
+    longitude = models.CharField(max_length=200, null=True)
+    latitude = models.CharField(max_length=200, null=True)
 
     #!find a way of grabbing the fuel station and representing it in a name here
     def __str__(self):
@@ -41,9 +51,11 @@ class Fuel_Station_Position (models.Model):
 
 
 class Fuel_Station_Extra_Information (models.Model):
-    number_of_votes_for_fuel_station_being_open  = models.IntegerField()
-    number_of_votes_for_fuel_station_being_close = models.IntegerField()
+    number_of_votes_for_fuel_station_being_open  = models.IntegerField(default=0)
+    number_of_votes_for_fuel_station_being_close = models.IntegerField(default=0)
 
     #!find a way of grabbing the fuel station and representing it in a name here
     def __str__(self):
         return self.name
+    
+    
