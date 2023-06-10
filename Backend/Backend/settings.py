d5e281080c7d3955a57a5145c66b4b89c54347a1
Sheryf Habib <sheryf.534@gmail.com>
2023-06-10 23:16:43 +0100

    Hosted django-cors

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -25,18 +25,22 @@ INSTALLED_APPS = [
     #  custom apps
     'Main',
     'Auth',
-    'Api'
+    'Api',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'Backend.urls'
 
