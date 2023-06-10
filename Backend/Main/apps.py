1f5b10f97fb1100128243512b6fc681497d3e4d7
Sheryf Habib <sheryf.534@gmail.com>
2023-06-10 09:53:15 +0100

    First test run of the django signals

--- Backend/Main/apps.py
+++ Backend/Main/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Main'
+
+    #? Initialize the signals
+    def ready(self):
+        import Main.signals
---
+++ Backend/Main/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save
+from .models import Fueling_station, Fuel_Station_Extra_Information, Fuel_Station_Position, Fuel_Station_Price, Fuel_Station_Traffic_Rating
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=Fueling_station)
+def save_fuel_station_related_information(sender, instance, created, **kwargs):
+    if created:
+        Fuel_Station_Extra_Information_instance = Fuel_Station_Extra_Information.objects.get_or_create(
+            station=instance)
+        Fuel_Station_Position_instance = Fuel_Station_Position.objects.get_or_create(
+            station=instance)
+        Fuel_Station_Price_instance = Fuel_Station_Price.objects.get_or_create(
+            station=instance)
+        Fuel_Station_Traffic_Rating_instance = Fuel_Station_Traffic_Rating.objects.get_or_create(
+            station=instance)
+
+
+        #? Save all instances created
+        Fuel_Station_Extra_Information_instance.save()
+        Fuel_Station_Position_instance.save()
+        Fuel_Station_Price_instance.save()
+        Fuel_Station_Traffic_Rating_instance.save()
