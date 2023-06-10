69573f8b95ca0521c6f1f317caf6de61acff5341
Sheryf Habib <sheryf.534@gmail.com>
2023-06-10 11:19:21 +0100

    Removed extra function because of recurssion error

--- Backend/Main/Extra_functions/model_methods.py
+++
@@ -1,8 +0,0 @@
-from Main.models import Fueling_station
-
-def get_fuel_station_instance(self):
-    id = self.station
-    try:
-        return Fueling_station.objects.get(id=id)
-    except:
-        pass
