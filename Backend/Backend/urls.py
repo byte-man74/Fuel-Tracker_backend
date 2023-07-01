87a9773619641dd00d85b0f224f295882b4e6f93
Sheryf Habib <sheryf.534@gmail.com>
2023-07-01 08:43:00 +0100

    urls staging for template

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',  include("Api.urls"))
+    path('api/',  include("Api.urls")),
+    path('', include("Main.urls"))
 ]
