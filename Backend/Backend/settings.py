c6bda86ecc4d22107917b23615730898bfa9cd54
Sheryf Habib <sheryf.534@gmail.com>
2023-07-02 12:14:23 +0100

    s3 connected

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django_filters',
     "crispy_forms",
     "crispy_bootstrap4",
+    'storages',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
@@ -178,14 +179,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -206,16 +199,25 @@ CORS_ALLOW_HEADERS = ['*']
 
 # AWS
 
-# Configure the AWS S3 settings
-AWS_ACCESS_KEY_ID = 'your-access-key-id'
-AWS_SECRET_ACCESS_KEY = 'your-secret-access-key'
-AWS_STORAGE_BUCKET_NAME = 'your-bucket-name'
-AWS_S3_REGION_NAME = 'your-region-name'  # e.g., 'us-east-1'
+USE_S3 =  'TRUE'
 
-# Set the location to store media files on AWS S3
-AWS_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{AWS_LOCATION}/'
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = 'AKIAUODQZ6U7PTHQ7MYX'
+    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_STORAGE_BUCKET_NAME = 'pricewiz'
+    AWS_DEFAULT_ACL = None
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    STATIC_URL = '/staticfiles/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
-# Configure the media files storage
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
