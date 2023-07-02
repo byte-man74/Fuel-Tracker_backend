95b11949d14daaef78add2e7f6dd36f05288ba97
Sheryf Habib <sheryf.534@gmail.com>
2023-07-02 01:20:51 +0100

    form styling

--- Backend/Main/views.py
+++ Backend/Main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import FuelingStationForm
 
 def create_fueling_station(request):
@@ -6,8 +7,11 @@ def create_fueling_station(request):
         form = FuelingStationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('fueling_station_list')  # Redirect to a success page
+            messages.success(request, 'Fueling station created successfully.')
+            return redirect('create_fueling_station')  # Redirect to the same page
     else:
         form = FuelingStationForm()
     
     return render(request, 'main/form.html', {'form': form})
+
+
