af261ad3a48e9df6e9dc459f3f74879b0b31e820
Sheryf Habib <sheryf.534@gmail.com>
2023-07-02 01:39:51 +0100

    more config and styling

--- Backend/Main/urls.py
+++ Backend/Main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import create_fueling_station
 
 urlpatterns = [
-    path('', create_fueling_station, name="Add Fueling Station")
+    path('', create_fueling_station, name="Add Fueling Station"),
+    
 ]
