3482bacb5f7dd8c670b192a7cde2ef9fb2dd55da
Ctrl-C-xCtrl-V <sheryf.534@gmail.com>
2023-07-09 15:30:01 +0100

    configured url

--- Backend/Main/urls.py
+++ Backend/Main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import create_fueling_station, fuel_station_position, success, landing_page
+from .views import create_fueling_station, fuel_station_position, success, landing_page, login_page, sign_up
 
 urlpatterns = [
     path('', landing_page, name="Home"),
+    path ('sign_up', sign_up, name="sign_up"),
+    path('login', login_page, name="login"),
     path('create_station', create_fueling_station, name="Add Fueling Station"),
     path('location/<int:fueling_station_id>', fuel_station_position , name="Fuel station position"),
     path('success', success, name="success_okay")
