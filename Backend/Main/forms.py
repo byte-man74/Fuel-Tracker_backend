98fcabfc61cb1f1a1f90a7533e4d0b505f64aa93
Ctrl-C-xCtrl-V <sheryf.534@gmail.com>
2023-07-09 16:46:57 +0100

    added login and sign up forms config

--- Backend/Main/forms.py
+++ Backend/Main/forms.py
@@ -1,13 +1,25 @@
 from django import forms
 from .models import Fueling_station, Fuel_Station_Position
+from django.contrib.auth.forms import UserCreationForm
+from Auth.models import CustomUser
 
 class FuelingStationForm(forms.ModelForm):
     class Meta:
         model = Fueling_station
         fields = ['name', 'address', 'local_government']
 
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
 
 class FuelingStationPositionForm (forms.ModelForm):
     class Meta:
         model = Fuel_Station_Position
         fields = ["longitude", "latitude"]
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password1', 'password2')
