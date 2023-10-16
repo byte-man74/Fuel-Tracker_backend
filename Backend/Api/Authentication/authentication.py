597a0111222e776d0b37c6454caea6513af2d2d3
Sheryf Habib <sheryf.534@gmail.com>
2023-10-16 17:43:33 +0100

    Authentication page clean up

--- Backend/Api/Authentication/authentication.py
+++ Backend/Api/Authentication/authentication.py
@@ -7,18 +7,8 @@ from Auth.models import CustomUser
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework import generics
-from django.shortcuts import get_object_or_404
-import time
-from django.core.serializers import serialize
 from rest_framework_simplejwt.views import TokenObtainPairView
 from Api.helper_functions.views_functions import *
-from rest_framework.permissions import IsAuthenticated
-from Auth.models import UserLocation
-from rest_framework import filters
-from rest_framework.exceptions import NotFound
-from Main.models import *
-from django.core.cache import cache
 from rest_framework.decorators import api_view
 
 
