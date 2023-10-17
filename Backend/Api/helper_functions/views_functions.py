87dc5229a3485ffbc7bc69470fa27b4af50efbb2
Sheryf Habib <sheryf.534@gmail.com>
2023-10-17 07:45:09 +0100

    view function staging

--- Backend/Api/helper_functions/views_functions.py
+++ Backend/Api/helper_functions/views_functions.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from Main.tasks import update_db, create_price_record
 import requests
-import math
+
 import requests
 import logging
 from django.conf import settings
@@ -241,19 +241,3 @@ def get_location_from_coordinates(latitude, longitude):
 
 
 
-def haversine(lat1, lon1, lat2, lon2):
-    # Convert latitude and longitude from degrees to radians
-    lat1_rad = math.radians(lat1)
-    lon1_rad = math.radians(lon1)
-    lat2_rad = math.radians(lat2)
-    lon2_rad = math.radians(lon2)
-
-    # Haversine formula
-    dlat = lat2_rad - lat1_rad
-    dlon = lon2_rad - lon1_rad
-    a = math.sin(dlat/2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon/2) ** 2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    earth_radius_km = 6371.0
-    distance_km = earth_radius_km * c
-
-    return distance_km
